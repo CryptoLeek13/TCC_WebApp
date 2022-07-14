39e7b56db61e95ca3042e2f4b0771c49df3463a0
Alan Brzuszko <alanbrzu@gmail.com>
2022-07-13 20:58:43 -0400

    changed some file structure

---
+++ src/components/DispoList/DispoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./TCCmainnew.png";
+import "./DispoList.css";
+
+export default function DispoList() {
+  return (
+    <div>
+      <header>
+        <nav>{logo}</nav>
+      </header>
+    </div>
+  );
+}
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './Search.css';
+import React from "react";
+import "./Search.css";
 
 const Search = () => {
   return (
-    <label>
-      <input
-        type="text"
-        placeholder="Enter Location"
-        className="search-input"
-      />
-    </label>
+    <form>
+      <div>
+        <label htmlFor="location"></label>
+        <input
+          id="location"
+          name="location"
+          type="text"
+          placeholder="Enter Location"
+          className="search-input"
+        />
+      </div>
+      <button type="submit" className="entry-button">
+        Enter
+      </button>
+    </form>
   );
 };
 export default Search;
