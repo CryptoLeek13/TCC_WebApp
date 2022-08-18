d8412e32c22862fd68c1bc9dd8e17346565277c9
Alan Brzuszko <alanbrzu@gmail.com>
2022-08-19 00:45:14 +0200

    opening page

--- src/components/Authorisation.js
+++ src/components/Authorisation.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import AgePopUp from './PopUps/AgePopUp';
-import Search from './Search/Search';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import AgePopUp from "./PopUps/AgePopUp";
+import Search from "./Search/Search";
+import { useNavigate } from "react-router-dom";
 
 function Authorisation() {
   const [allow, setAllow] = React.useState(false);
-  const [location, setLocation] = React.useState('');
+  const [location, setLocation] = React.useState("");
   const [activate, setActivate] = React.useState(false);
 
   const navigate = useNavigate();
 
-  const local = localStorage.getItem('location');
-  const age = localStorage.getItem('age');
+  const local = localStorage.getItem("location");
+  const age = localStorage.getItem("age");
 
   React.useEffect(() => {
     if (activate) {
-      localStorage.setItem('location', location);
-      navigate('/home', { state: location });
+      localStorage.setItem("location", location);
+      navigate("/home", { state: location });
     }
   }, [activate, location, navigate]);
 
   const saveAge = () => {
-    localStorage.setItem('age', allow);
+    localStorage.setItem("age", allow);
   };
 
   React.useEffect(() => {
     if (local != null && age) {
-      navigate('/home', { state: local });
+      navigate("/home", { state: local });
     }
   });
   return (
     <div className="container">
       <img
-        src={require('../tempIMG/ccLandscape2-day.png')}
+        src={require("../tempIMG/kronikLogo.png")}
         alt="logo"
         aria-hidden="true"
         className="entry-logo-S"
       />
       <img
-        src={require('../tempIMG/ccLandscape1-day.png')}
+        src={require("../tempIMG/kronikLogo.png")}
         alt="logo"
         aria-hidden="true"
         className="entry-logo-X"
